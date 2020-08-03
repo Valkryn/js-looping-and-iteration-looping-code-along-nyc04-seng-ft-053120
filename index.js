dc233a9597d9ba5a5251af5d7e4069d3d653d178
Valkryn <calvinraphaelg@gmail.com>
2020-08-03 19:05:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+function writeCards (array , event ) {
+  let thankYou = []
+for (let i = 0; i < array.length; i++ ){
+  thankYou.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+}
+return thankYou
+}
+
+
+
+
+
+
+let x = 10;
+function countDown(integer){
+  while (integer > 0){
+    console.log (integer);
+    integer = integer - 1;
+}
+console.log(integer)
+}
+countDown(x)
